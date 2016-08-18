94076fee686b09a12a604b04e706d55aee05b4a7
joshuaavalos74 <joshuaavalos74@gmail.com>
2016-08-18 08:43:09 -0600

    oniszdioxcvbnófd

---
+++ ficha.py
@@ -0,0 +1,3 @@
+from fichas import Fichasas
+class Ficha(Fichasas):
+	def __init__(self)
---
+++ fichas.py
@@ -0,0 +1,7 @@
+class Fichasas(object):
+	def __init__(self, lugar, puesto):
+		self.lugar = lugar
+		self.puesto= False
+		
+
+
